0cd1056862af287f120a5fd45a4cd5c3b758543c
charlesoma <charlyoma007@gmail.com>
2020-02-11 23:10:29 +0100

    Updates

---
+++ precache-manifest.80422bada1fcd1314436f84bbe2ced8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4911bac1434c471385041b4096385a5",
+    "url": "/birectory/index.html"
+  },
+  {
+    "revision": "2fc715e1234c02caeeec",
+    "url": "/birectory/static/css/main.af74555f.chunk.css"
+  },
+  {
+    "revision": "b2eb3de0c547c7309c6d",
+    "url": "/birectory/static/js/2.090ecc1a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/birectory/static/js/2.090ecc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc715e1234c02caeeec",
+    "url": "/birectory/static/js/main.d9e87267.chunk.js"
+  },
+  {
+    "revision": "4c16f62b16fba3e73725",
+    "url": "/birectory/static/js/runtime-main.fb1a0033.js"
+  }
+]);
